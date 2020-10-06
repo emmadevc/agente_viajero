2383de29fd55189ed018763172240a1b59d60f6f
emmadevc <emmanuel_tm@outlook.com>
2020-10-05 21:53:51 -0500

    Create pintar.php

---
+++ pintar.php
@@ -0,0 +1,146 @@
+<?php 
+    $estados[0][0]=5000;
+    $estados[0][1]=1995;
+    $estados[0][2]=2389;
+    $estados[0][3]=1896;
+    $estados[0][4]=2099;
+    $estados[0][5]=2600;
+    $estados[0][6]=2457;
+    $estados[0][7]=2809;
+    $estados[0][8]=3259;
+    $estados[0][9]=4120;
+
+    $estados[1][0]=2178;
+    $estados[1][1]=5000;
+    $estados[1][2]=516;
+    $estados[1][3]=1048;
+    $estados[1][4]=796;
+    $estados[1][5]=707;
+    $estados[1][6]=922;
+    $estados[1][7]=909;
+    $estados[1][8]=1362;
+    $estados[1][9]=2221;
+
+    $estados[2][0]=2389;
+    $estados[2][1]=516;
+    $estados[2][2]=5000;
+    $estados[2][3]=534;
+    $estados[2][4]=333;
+    $estados[2][5]=213;
+    $estados[2][6]=428;
+    $estados[2][7]=415;
+    $estados[2][8]=868;
+    $estados[2][9]=1727;
+
+    $estados[3][0]=1846;
+    $estados[3][1]=1050;
+    $estados[3][2]=536;
+    $estados[3][3]=5000;
+    $estados[3][4]=206;
+    $estados[3][5]=586;
+    $estados[3][6]=763;
+    $estados[3][7]=740;
+    $estados[3][8]=1203;
+    $estados[3][9]=2061;
+
+    $estados[4][0]=2051;
+    $estados[4][1]=793;
+    $estados[4][2]=340;
+    $estados[4][3]=206;
+    $estados[4][4]=5000;
+    $estados[4][5]=386;
+    $estados[4][6]=564;
+    $estados[4][7]=541;
+    $estados[4][8]=1003;
+    $estados[4][9]=1862;
+
+    $estados[5][0]=2603;
+    $estados[5][1]=704;
+    $estados[5][2]=213;
+    $estados[5][3]=585;
+    $estados[5][4]=386;
+    $estados[5][5]=5000;
+    $estados[5][6]=227;
+    $estados[5][7]=214;
+    $estados[5][8]=666;
+    $estados[5][9]=1525;
+
+    $estados[6][0]=2409;
+    $estados[6][1]=922;
+    $estados[6][2]=429;
+    $estados[6][3]=765;
+    $estados[6][4]=564;
+    $estados[6][5]=229;
+    $estados[6][6]=5000;
+    $estados[6][7]=96;
+    $estados[6][8]=480;
+    $estados[6][9]=1339;
+
+    $estados[7][0]=2623;
+    $estados[7][1]=910;
+    $estados[7][2]=417;
+    $estados[7][3]=740;
+    $estados[7][4]=541;
+    $estados[7][5]=218;
+    $estados[7][6]=94;
+    $estados[7][7]=5000;
+    $estados[7][8]=465;
+    $estados[7][9]=1325;
+
+    $estados[8][0]=3043;
+    $estados[8][1]=1358;
+    $estados[8][2]=864;
+    $estados[8][3]=1200;
+    $estados[8][4]=1002;
+    $estados[8][5]=665;
+    $estados[8][6]=480;
+    $estados[8][7]=463;
+    $estados[8][8]=5000;
+    $estados[8][9]=1376;
+
+    $estados[9][0]=3914;
+    $estados[9][1]=2226;
+    $estados[9][2]=1733;
+    $estados[9][3]=2069;
+    $estados[9][4]=1870;
+    $estados[9][5]=1533;
+    $estados[9][6]=1348;
+    $estados[9][7]=1331;
+    $estados[9][8]=1386;
+    $estados[9][9]=5000;
+
+
+    $posicion[0][0]="65px";
+    $posicion[0][1]="5px";
+
+    $posicion[1][0]="647px";
+    $posicion[1][1]="309px";
+
+    $posicion[2][0]="614px";
+    $posicion[2][1]="450px";
+
+    $posicion[3][0]="453px";
+    $posicion[3][1]="482px";
+
+    $posicion[4][0]="518px";
+    $posicion[4][1]="516px";
+
+    $posicion[5][0]="645px";
+    $posicion[5][1]="519px";
+
+    $posicion[6][0]="713px";
+    $posicion[6][1]="535px";
+
+    $posicion[7][0]="697px";
+    $posicion[7][1]="569px";
+
+    $posicion[8][0]="798px";
+    $posicion[8][1]="663px";
+
+    $posicion[9][0]="1152px";
+    $posicion[9][1]="573px";
+
+    echo '<line x1="65" y1="5" x2="647" y2="309" id="id0"></line>
+    <line x1="713" y1="535" x2="697" y2="569" stroke="black" stroke-width="8"></line>
+    <circle cx="697" cy="569" r="8" id="c0"/>';
+?>
